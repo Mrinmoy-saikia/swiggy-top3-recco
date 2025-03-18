eeac055994a649e9ca63ab4d431025c7ff979f3f
Mrinmoy-saikia <Mrinmoyjsaikia@gmail.com>
2025-03-18 19:33:09 +0530

    project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It will help to reccomend on top 3 item on swiggy',
+    author='Mrinmoy',
+    license='',
+)
